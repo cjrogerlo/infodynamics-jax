7dd1011979910db5c04c8772493b62b1a51e9693
cjrogerlo <cjrogerlo@cjrogerlodeMacBook-Air.local>
2026-01-01 04:05:45 +0000

    Register gaussian likelihood

--- infodynamics_jax/likelihoods/__init__.py
+++ infodynamics_jax/likelihoods/__init__.py
@@ -4,10 +4,12 @@ from .bernoulli import bernoulli
 from .poisson import poisson
 from .negative_binomial import negative_binomial
 from .ordinal import ordinal
+from .gaussian import gaussian
 
 register("bernoulli", bernoulli)
 register("poisson", poisson)
 register("negative_binomial", negative_binomial)
 register("ordinal", ordinal)
+register("gaussian", gaussian)
 
 __all__ = ["get"]
