e9d118faa35a635174b823d83f579a336ffbb168
cjrogerlo <cjrogerlo@cjrogerlodeMacBook-Air.local>
2026-01-01 03:33:06 +0000

    Expose likelihood registry API (register/get)

--- infodynamics_jax/likelihoods/__init__.py
+++ infodynamics_jax/likelihoods/__init__.py
@@ -1,13 +1,22 @@
-from .gaussian import GaussianLikelihood
-from .bernoulli import BernoulliLikelihood
-from .poisson import PoissonLikelihood
-from .negative_binomial import NegativeBinomialLikelihood
-from .ordinal import OrdinalLikelihood
+# infodynamics_jax/likelihoods/__init__.py
+
+from .base import register, get
+
+from .bernoulli import bernoulli
+from .poisson import poisson
+from .negative_binomial import negative_binomial
+from .ordinal import ordinal
+
+register("bernoulli", bernoulli)
+register("poisson", poisson)
+register("negative_binomial", negative_binomial)
+register("ordinal", ordinal)
 
 __all__ = [
-    "GaussianLikelihood",
-    "BernoulliLikelihood",
-    "PoissonLikelihood",
-    "NegativeBinomialLikelihood",
-    "OrdinalLikelihood",
+    "get",
+    "bernoulli",
+    "poisson",
+    "negative_binomial",
+    "ordinal",
 ]
+
